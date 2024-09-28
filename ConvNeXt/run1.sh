d6e31fde93942d26a0cc8d309b9c3880e87064d9
dylanli <dylan.h.li@outlook.com>
2024-09-28 19:21:38 +0000

    update

--- ConvNeXt/run1.sh
+++ ConvNeXt/run1.sh
@@ -5,7 +5,7 @@ NUM_GPUS=4
 TRAIN_FUNC=$2
 
 
-python -m torch.distributed.launch --nproc_per_node=4 main.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 main.py \
   --model convnext_tiny --drop_path 0.1 \
   --batch_size 128 --lr 4e-3 --update_freq 8 \
   --model_ema true --model_ema_eval true \
