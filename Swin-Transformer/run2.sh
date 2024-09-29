d61e76c65d6dc4c4d3ba107c67b42016db898ce8
dylanli <dylan.h.li@outlook.com>
2024-09-29 21:13:47 +0000

    update

--- Swin-Transformer/run2.sh
+++ Swin-Transformer/run2.sh
@@ -5,7 +5,7 @@ NUM_GPUS=4
 mkdir -p "${OUTPUT_DIR}/${EXPERIMENT_NAME}"
 touch "${OUTPUT_DIR}/${EXPERIMENT_NAME}/outputs.txt"
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node 4 --master_port 12345  main.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node 4 --master_port 12346  main.py \
     --cfg configs/swin/swin_tiny_patch4_window7_224.yaml \
     --data-path "${DATA_PATH}" \
     --batch-size 256 \
