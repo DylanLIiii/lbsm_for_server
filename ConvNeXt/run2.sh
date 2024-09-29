218e360c5baaf408bb62a37eb55f58722358fa88
dylanli <dylan.h.li@outlook.com>
2024-09-29 21:20:11 +0000

    update

--- ConvNeXt/run2.sh
+++ ConvNeXt/run2.sh
@@ -7,7 +7,7 @@ TRAIN_FUNC=$2
 mkdir -p "${OUTPUT_DIR}/${EXPERIMENT_NAME}"
 touch "${OUTPUT_DIR}/${EXPERIMENT_NAME}/outputs.txt"
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 main.py \
+CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port 12347 main.py \
 --model convnext_tiny --drop-path 0.1 \
 --batch-size 128 --lr 4e-3 --update-freq 8 \
 --model-ema true --model-ema-eval true \
